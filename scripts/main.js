42c53fd896abefa4e34e3a94439a263277b03f64
Se7venlabs <darryl.west@gmail.com>
2026-04-28 08:00:47 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,20 @@
+import { COMPONENTS } from '../config/settings.js';
 
+async function loadComponent(id, path) {
+  const el = document.getElementById(id);
+  if (!el) return;
+
+  const res = await fetch(path);
+  const html = await res.text();
+  el.innerHTML = html;
+}
+
+async function init() {
+  for (const comp of COMPONENTS) {
+    await loadComponent(comp.id, comp.path);
+  }
+
+  document.dispatchEvent(new Event('components:loaded'));
+}
+
+init();
